8ec6fa0d4dc99c6f089b32e0dd29f9688a8fdecd
Serge Lerner <sergeinbox@gmail.com>
2016-01-03 19:16:20 +0200

    enable configureStore

--- app/index.js
+++ app/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers/index.reducer';
 import Reserved from './containers/reserved.container';
+import configureStore from './store/configureStore'
 import styles from './style/style.scss';
 
-const store = createStore(rootReducer);
-
 render(
-  <Provider store={store}>
+  <Provider store={configureStore()}>
     <Reserved />
   </Provider>,
   document.getElementById('root')
--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,18 +1,18 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import reducer from '../reducers'
+import rootReducer from '../reducers/index.reducer';
 
 const createStoreWithMiddleware = applyMiddleware(
   thunk
 )(createStore)
 
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(reducer, initialState)
+  const store = createStoreWithMiddleware(rootReducer, initialState)
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers')
+    module.hot.accept('../reducers/index.reducer', () => {
+      const nextReducer = require('../reducers/index.reducer')
       store.replaceReducer(nextReducer)
     })
   }
